f4eaaa51a7ab804ebb55cee61ac064a19693b767
Rohin Goyal <rohingoyal456@gmail.com>
2021-08-05 23:59:25 +0530

    bug: code update

--- Level 1/Recursion/Introductory Problems/factorial.java
+++ Level 1/Recursion/Introductory Problems/factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Factorial_using_recursion {
+public class factorial {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the number:");
